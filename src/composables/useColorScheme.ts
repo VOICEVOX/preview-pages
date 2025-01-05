df37fa74e2f10a100663363dbf41e7cbcdb4a690
Hiroshiba Kazuyuki <kazuyuki_hiroshiba@dwango.co.jp>
2025-01-05 16:01:22 +0900

    Fix: useColorSchemeでのテーマ変更時のイベントリスナーの追加と削除を実装

--- src/composables/useColorScheme.ts
+++ src/composables/useColorScheme.ts
@@ -1,18 +1,24 @@
-import { onMounted } from "vue";
+import { onMounted, onUnmounted } from "vue";
 
 export function useColorScheme() {
+  const setTheme = (isDark: boolean) => {
+    if (isDark) {
+      document.documentElement.classList.add("dark");
+      document.documentElement.classList.remove("light");
+    } else {
+      document.documentElement.classList.remove("dark");
+      document.documentElement.classList.add("light");
+    }
+  };
+
+  const colorScheme = window.matchMedia("(prefers-color-scheme: dark)");
+  const setColorScheme = (e: MediaQueryListEvent) => setTheme(e.matches);
+
   onMounted(() => {
-    const colorScheme = window.matchMedia("(prefers-color-scheme: dark)");
-    const setTheme = (isDark: boolean) => {
-      if (isDark) {
-        document.documentElement.classList.add("dark");
-        document.documentElement.classList.remove("light");
-      } else {
-        document.documentElement.classList.remove("dark");
-        document.documentElement.classList.add("light");
-      }
-    };
-    colorScheme.addEventListener("change", (e) => setTheme(e.matches));
     setTheme(colorScheme.matches);
+    colorScheme.addEventListener("change", setColorScheme);
+  });
+  onUnmounted(() => {
+    colorScheme.removeEventListener("change", setColorScheme);
   });
 }
